c73569d93c82e5c9de98e258ff80d646c957ea1d
akmal <akmal@brighter.com>
2014-12-17 15:30:20 -0800

    all variables at beginning and db conn renamed

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var app = express();
+var express = require('express'), 
+  app = express(),
+  logger = require('nlogger').logger(module),
+  nconf = require('./config'),
+  conn = require('./db');
+
 require('./middlewares')(app);
 require('./router')(app);
 
-var logger = require('nlogger').logger(module);
-var nconf = require('./config');
-
-var database = require('./db');
-database.on('error', console.error.bind(console, 'connection error:'));
-database.once('open', function callback () {
+conn.on('error', function(err) {
+  logger.error('Error connecting to the database: ', err)
+});
+conn.once('open', function callback () {
   logger.info("Connected to telegramDb");
   app.listen(3000, function() {
     logger.info('Server started');
