6cf36da08b5b8806101e17dc3107659fb67cfe68
akmal <akmal@brighter.com>
2014-12-17 16:20:37 -0800

    align variables and rename userModel to User

--- auth/index.js
+++ auth/index.js
@@ -1,21 +1,19 @@
 var passport = require('passport');
 var passportLocal = require('passport-local');
 var logger = require('nlogger').logger(module);
-
-var db = require('../db');
-var userModel = db.model('user');
+var User = require('../db').model('user');
+var localStrategy = require('./strategy/local');
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  userModel.findOne({id: id}, function(err, doc){
+  User.findOne({id: id}, function(err, doc){
     done(null, doc);
   });
 });
 
-var localStrategy = require('./strategy/local');
 passport.use(new passportLocal.Strategy(localStrategy));
 
 module.exports = passport;
