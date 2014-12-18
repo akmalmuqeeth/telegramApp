c2dbb2eff9cf17ebc9b590378b736d672c671bc7
akmal <akmal@brighter.com>
2014-12-17 16:18:16 -0800

    expand one line if statements

--- auth/strategy/local.js
+++ auth/strategy/local.js
@@ -5,8 +5,13 @@ var bcrypt = require('bcrypt');
 
 module.exports = function (username, password, done) {
   userModel.findOne({id : username}, function(err, user) {
-    if (err) return done(err, null);
-    if (!user) return done(null,null, 'user not found');
+    if (err) {
+      logger.error('Error finding user with id: ', username, ' Err: ', err);
+      return done(err);
+    }
+    if (!user) { 
+      return done(null,null, 'user not found');
+    }
 
     bcrypt.compare(password, user.password, function(err, res) {
       if (res) return done(null, user);      
